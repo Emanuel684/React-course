e549f14307d050651511fe19c0cc8fcb8177f2e0
Emanuel Acevedo <emanuel.acevedo@ciclo.com.co>
2021-09-21 15:32:50 -0500

    21/09/2021 Simple Form

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 
 // import HooksApp from './HooksApp';
 // import CounterApp from './components/01-useState/CounterApp';
-import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
+// import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
+import SimpleForm from './components/02-useEffect/SimpleForm';
 
 ReactDOM.render(
     // <HooksApp />,
     // <CounterApp />,
-    <CounterWithCustomHook />,
+    // <CounterWithCustomHook />,
+    <SimpleForm />,
   document.getElementById('root')
 );
